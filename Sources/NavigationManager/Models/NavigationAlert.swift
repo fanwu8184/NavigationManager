7303488959ea465b33ad102add376ccb3aa76fb3
fan-wu <fan.wu@rea-group.com>
2026-01-16 18:02:49 +1100

    Refactor NavigationAlert to remove unnecessary public access modifiers

--- Sources/NavigationManager/Models/NavigationAlert.swift
+++ Sources/NavigationManager/Models/NavigationAlert.swift
@@ -2,13 +2,13 @@ import SwiftUI
 
 /// Represents an alert to be presented by the navigation manager.
 struct NavigationAlert: Identifiable {
-  public let id: String
-  public let title: String
-  public let actions: AnyView
-  public let message: AnyView?
+  let id: String
+  let title: String
+  let actions: AnyView
+  let message: AnyView?
 
   /// Initializes a navigation alert with a title, actions, and a message.
-  public init<A: View, M: View>(
+  init<A: View, M: View>(
     title: String,
     @ViewBuilder actions: () -> A,
     @ViewBuilder message: () -> M
@@ -20,7 +20,7 @@ struct NavigationAlert: Identifiable {
   }
 
   /// Initializes a navigation alert with a title and actions.
-  public init<A: View>(
+  init<A: View>(
     title: String,
     @ViewBuilder actions: () -> A
   ) {
